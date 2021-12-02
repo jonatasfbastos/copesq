363d9a4d116b1fb54342e44cd4a40956d02ee975
rafaeldss <rafaeldss199916@gmail.com>
2021-12-02 15:58:36 -0300

    Cria models

---
+++ src/main/java/br/com/ifba/copesq/tecnico_administrativo/model/TecnicoAdministrativo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifba.copesq.tecnico_administrativo.model;
+
+import br.com.ifba.copesq.infrastructure.model.PersistenceEntity;
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author rafael
+ */
+@Entity
+public class TecnicoAdministrativo extends PersistenceEntity implements Serializable {
+    
+}
